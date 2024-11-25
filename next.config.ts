c0782600b2d6df7799e2fa85eeff7f4a724caeab
Muhammad Murtaza <muhammad.murtaza5253@gmail.com>
2024-11-25 12:54:51 +0500

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*', // Match all routes
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/html;', // Default Content-Type for HTML responses
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

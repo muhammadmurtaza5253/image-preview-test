06981a474c3ed73e53bf86215212e2a1a16cf2a1
Muhammad Murtaza <muhammad.murtaza5253@gmail.com>
2024-11-25 11:26:22 +0500

    added a large script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta property="og:title" content="Amazing Image Preview" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossOrigin="anonymous" />
+        <script type="text/javascript" src="scripts/large-script.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossOrigin="anonymous"></script>
+        <meta charSet="utf-8" /><meta name="viewport" content="width=device-width" />
+        <link rel="preconnect" href="https://tags.crwdcntrl.net" />
+        <link rel="preconnect" href="https://bcp.crwdcntrl.net" />
+        <link rel="preconnect" href="https://c.ltmsphrcl.net" />
+        <link rel="dns-prefetch" href="https://tags.crwdcntrl.net" />
+        <link rel="dns-prefetch" href="https://bcp.crwdcntrl.net" />
+        <link rel="dns-prefetch" href="https://c.ltmsphrcl.net" />
+        <meta property="og:title" content="Amazing Image Preview" />
         <meta property="og:description" content="Check out this amazing image!" />
         <meta
           property="og:image"
---
+++ src/app/scripts/large-script.js
@@ -0,0 +1,4 @@
+
+// This is a large script file to simulate heavy script imports in an HTML document.
+const largeData = "A".repeat(50000); // 50 KB of data
+console.log("Large script loaded successfully");
